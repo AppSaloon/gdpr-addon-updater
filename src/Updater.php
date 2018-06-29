45531e4b49dca1c392de18a520dd4dce4bb4cba3
Kim Vanrijckel <vanrijckel@gmail.com>
2018-06-29 16:56:28 +0200

    check if GDPR_UPDATE_DIR

--- src/Updater.php
+++ src/Updater.php
@@ -7,8 +7,9 @@ use BitbucketUpdater\Lib\AutoUpdateLegacy;
 use BitbucketUpdater\Model\Bitbucket;
 use BitbucketUpdater\Model\Edd;
 use BitbucketUpdater\Controller\LicenseController;
-
-define('GDPR_UPDATER_DIR', trailingslashit( dirname( __FILE__ ) ));
+if ( ! defined ('GDPR_UPDATER_DIR' ) ) {
+	define( 'GDPR_UPDATER_DIR', trailingslashit( dirname( __FILE__ ) ) );
+}
 
 class Updater {
 
